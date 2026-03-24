3a74d651c3928a14b071b44c1ef679d1662efdb4
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-24 15:36:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Banner from './Component/Home/Banner/Banner'
+import Navbar from './Component/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+     <Navbar></Navbar>
+     <Banner></Banner>
+    </>
+  )
+}
+
+export default App
