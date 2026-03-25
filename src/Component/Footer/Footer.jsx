da32d48d4c20054cd4cfdaf3f55a4b60502b572f
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-25 16:12:12 +0600

    added footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,29 +4,31 @@ const Footer = () => {
     return (
         <div className='bg-[#06091A]'>
             <div className="">
-                <img className='mx-auto' src={footerImg} alt="" />
-                <footer className="grid grid-cols-3 container mx-auto footer sm:footer-horizontal text-base-content p-10">
-                    <nav className='text-white'>
-                        <h6 className="footer-title text-2xl">About Us</h6>
+                <img className='mx-auto px-4' src={footerImg} alt="" />
+                <footer className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 container mx-auto px-6 py-10 text-base-content">
+                    <nav className='text-white text-center sm:text-left'>
+                        <h6 className="footer-title text-2xl mb-2">About Us</h6>
                         <p className='text-gray-400'>We are a passionate team <br /> dedicated to providing the best <br /> services to our customers.</p>
                     </nav>
-                    <nav className='text-gray-400'>
-                        <h6 className="footer-title text-2xl text-gray-50">Quick Links</h6>
-                        <a className="link link-hover">Home</a>
-                        <a className="link link-hover">Services</a>
-                        <a className="link link-hover">About</a>
-                        <a className="link link-hover">Contact</a>
+                    <nav className='text-gray-400 text-center sm:text-left'>
+                        <h6 className="footer-title text-2xl text-gray-50 mb-2">Quick Links</h6>
+                        <div className='flex flex-col gap-1'>
+                            <a className="link link-hover">Home</a>
+                            <a className="link link-hover">Services</a>
+                            <a className="link link-hover">About</a>
+                            <a className="link link-hover">Contact</a>
+                        </div>
                     </nav>
 
-                    <form className='text-left'>
-                        <h6 className="footer-title text-white text-2xl">Subscribe</h6>
-                        <fieldset className="w-80">
+                    <form className='text-center sm:text-left'>
+                        <h6 className="footer-title text-white text-2xl mb-2">Subscribe</h6>
+                        <fieldset className="w-full max-w-sm mx-auto sm:mx-0">
                             <label className='text-gray-400'>Subscribe to our newsletter <br /> for the latest updates.</label>
-                            <div className="join mt-7">
+                            <div className="join mt-5 w-full">
                                 <input
                                     type="text"
                                     placeholder="username@site.com"
-                                    className="input input-bordered join-item rounded-l-2xl" />
+                                    className="input input-bordered join-item w-full rounded-l-2xl" />
                                 <button className="btn btn-primary join-item rounded-r-2xl">Subscribe</button>
                             </div>
                         </fieldset>
@@ -34,9 +36,9 @@ const Footer = () => {
                 </footer>
 
             </div>
-            <div className="divider h-[1px] m-0 bg-gray-500"></div>
-            <div className='text-center py-3'>
-                <p className='text-gray-400'>@2024 Your Company All Rights Reserved.</p>
+            <div className="h-[1px] bg-gray-500"></div>
+            <div className='text-center py-4 px-4'>
+                <p className='text-gray-400 text-sm sm:text-base'>@2024 Your Company All Rights Reserved.</p>
             </div>
         </div>
     );
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from "../../assets/logo.png"
 import dollerImg from "../../assets/DoubleDollar.png"
 
-const Navbar = ({coin}) => {
+const Navbar = ({ coin }) => {
     return (
-        <div className='container mx-auto'>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className=' bg-base-100 shadow-sm'>
+            <div className="navbar container mx-auto mb-7">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -13,32 +14,25 @@ const Navbar = ({coin}) => {
                         <ul
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            <ul className="menu menu-horizontal px-1">
+                                <li><a>Home</a></li>
+                                <li><a>Fixture</a></li>
+                                <li><a>Teams</a></li>
+                                <li><a>Schedules</a></li>
+
+                            </ul>
+
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a><img src={logo} alt="" /></a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2 bg-base-100 w-40 z-1">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Home</a></li>
+                        <li><a>Fixture</a></li>
+                        <li><a>Teams</a></li>
+                        <li><a>Schedules</a></li>
+
                     </ul>
                 </div>
                 <div className="navbar-end">
